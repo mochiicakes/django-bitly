4b81e4e8eccb6189f092369c8190d7871648d7c2
mochiicakes <michikokin.iris@gmail.com>
2025-01-23 14:27:47 +0800

    enabled hosts for deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h+cs68*+-cobx1_7a@lr7-(mf6s7w#ahc^v86rhx3$@hx+vo-1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
